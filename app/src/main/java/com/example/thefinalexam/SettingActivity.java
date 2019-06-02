e57f63295fd49dea715a3cdea797acad0f2d66dc
jimdotlin <jim95642123@gmail.com>
2019-06-02 22:28:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/thefinalexam/SettingActivity.java
@@ -0,0 +1,20 @@
+package com.example.thefinalexam;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
